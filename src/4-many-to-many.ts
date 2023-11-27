ab7328880560863acea2618d510ac509230c1dd6
Sumon Chandra <topu199041@gmail.com>
2023-11-27 14:59:18 +0600

    many-to-many relationship that call itself

--- src/4-many-to-many.ts
+++ src/4-many-to-many.ts
@@ -8,3 +8,51 @@ import fs from "fs";
  * A category can have a parent category and a category can have multiple child categories
  */
 
+async function createCategories() {
+    await clean()
+
+    const categoriesPromises = Array(10).fill(null).map(() => {
+        return prisma.category.create({
+            data: {
+                name: faker.commerce.productAdjective()
+            }
+        })
+    })
+
+    const categories = await prisma.$transaction(categoriesPromises);
+
+    const childCategoriesPromises = Array(10).fill(null).map((_, i) => {
+        return prisma.category.create({
+            data: {
+                name: faker.commerce.productAdjective(),
+                parent: {
+                    connect: {
+                        id: categories[i].id,
+                    }
+                }
+            }
+        })
+    })
+
+    return prisma.$transaction(childCategoriesPromises)
+}
+
+function findCategories() {
+    return prisma.category.findMany({
+        include: {
+            parent: true,
+            children: true
+        }
+    })
+}
+
+const childCategories = JSON.stringify(await createCategories(), null, 2)
+const categories = JSON.stringify(await findCategories(), null, 2)
+console.log("many-to-many that call itself");
+console.log(childCategories);
+console.log("--------------------------------");
+console.log("Find categories");
+console.log(categories);
+
+
+
