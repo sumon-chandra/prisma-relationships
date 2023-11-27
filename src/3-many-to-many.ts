ed8d907bf92f95a423ff15a3ae6a35afe9c6c8d1
Sumon Chandra <topu199041@gmail.com>
2023-11-27 14:39:50 +0600

    many-to-many relationship

--- src/3-many-to-many.ts
+++ src/3-many-to-many.ts
@@ -8,3 +8,47 @@ import { sampleArray } from "./helpers.ts/random";
  * A product can have multiple categories and a category can belong to multiple products
  */
 
+async function createProductsWithCategories() {
+    await clean()
+
+    const categoriesPromises = Array(10).fill(null).map(() => {
+        return prisma.category.create({
+            data: {
+                name: faker.commerce.productAdjective()
+            }
+        })
+    })
+
+    const categories = await prisma.$transaction(categoriesPromises);
+
+    const productsPromises = Array(10).fill(null).map(() => {
+        return prisma.product.create({
+            data: {
+                name: faker.commerce.productName(),
+                description: faker.commerce.productDescription(),
+                price: parseInt(faker.commerce.price()),
+                category: {
+                    connect: [
+                        {
+                            id: categories[Math.floor(Math.random() * categories.length)].id
+                        },
+                        {
+                            id: categories[Math.floor(Math.random() * categories.length)].id
+                        }
+                    ]
+                }
+            },
+            include: {
+                category: true
+            }
+        })
+    })
+
+    return prisma.$transaction(productsPromises)
+}
+
+const productsWithCategories = JSON.stringify(await createProductsWithCategories(), null, 2)
+
+console.log("many-to-many");
+console.log(productsWithCategories);
+
